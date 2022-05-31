e9f7111152db99bfa5b58fc70e3a14a9c09a1342
Karen1424 <vip.karengrigoryan@mail.ru>
2022-05-31 14:11:27 +0400

    syntax correction

--- contracts/Distributer.sol
+++ contracts/Distributer.sol
@@ -27,15 +27,9 @@ contract Distributer is Ownable {
   
     mapping(address => Owners) public owners;
      
-    enum OwnerStatus {
-		Active,
-        Frozen
-	}
+    enum OwnerStatus { Active, Frozen }
 
-    enum OwnerType {
-        Founder,
-        Employee
-    }
+    enum OwnerType { Founder, Employee }
 
     struct Owners {
         OwnerStatus status;
@@ -48,64 +42,66 @@ contract Distributer is Ownable {
     }
 
     /// Owner Only functions
-    function addFounder(address founder,uint256 amount) public onlyOwner {
-        require(owners[founder].owner == address(0), "on this address Founder already exists");
-        require(foundersTotalSupply >= amount,"Can not add a new founder because the tokens are exhausted");
-        Owners memory owner = Owners(
-            OwnerStatus.Frozen,
-            OwnerType.Founder,
-            founder,
-            (amount - (amount / 5)),
-            (amount / 5),
-            (block.timestamp + 730 days),
-            (block.timestamp + 730 days)
-        );
+    function addFounder(address founder_, uint256 amount_) public onlyOwner {
+
+        require(owners[founder_].owner == address(0), "on this address Founder already exists");
+        require(foundersTotalSupply >= amount_, "Can not add a new founder because the tokens are exhausted");
+        Owners memory owner = Owners(OwnerStatus.Frozen,
+                                     OwnerType.Founder,
+                                     founder_,
+                                     (amount_ - (amount_ / 5)),
+                                     (amount_ / 5),
+                                     (block.timestamp + 730 days),
+                                     (block.timestamp + 730 days));
         
-        owners[founder] = owner;
-        foundersTotalSupply -= amount;   
+        owners[founder_] = owner;
+        foundersTotalSupply -= amount_;   
     } 
 
-    function addEmployee(address employee, uint256 amount) public onlyOwner {
-        require(owners[employee].owner == address(0), "on this address employee already exists");
-        require(employeesTotalSupply >= amount,"Can not add a new employee because the tokens are exhausted");
-        Owners memory owner = Owners(
-            OwnerStatus.Frozen,
-            OwnerType.Employee,
-            employee,
-            (amount - (amount / 10)),
-            (amount / 10),
-            (block.timestamp + 180 days),
-            (block.timestamp + 180 days)
-        );
+    function addEmployee(address employee_, uint256 amount_) public onlyOwner {
 
-        owners[employee] = owner;
-        employeesTotalSupply -= amount;
+        require(owners[employee_].owner == address(0), "on this address employee already exists");
+        require(employeesTotalSupply >= amount_, "Can not add a new employee because the tokens are exhausted");
+        Owners memory owner = Owners(OwnerStatus.Frozen,
+                                     OwnerType.Employee,
+                                     employee_,
+                                     (amount_ - (amount_ / 10)),
+                                     (amount_ / 10),
+                                     (block.timestamp + 180 days),
+                                     (block.timestamp + 180 days));
+
+        owners[employee_] = owner;
+        employeesTotalSupply -= amount_;
     }
 
-    function soldTokensManagement(address buyer, uint256 amount) public payable onlyOwner {
+    function soldTokensManagement(address buyer_, uint256 amount_) public payable onlyOwner {
+
         require(msg.sender != distributerOwner, "Distributer can not be buyer");
-        require(amount >= soldTokensTotalSupply,"should not exceed the total balance of tokens sold");
-        fastToken.transfer(buyer, amount);
-        soldTokensTotalSupply -= amount;
+        require(amount_ >= soldTokensTotalSupply, "should not exceed the total balance of tokens sold");
+        fastToken.transfer(buyer_, amount_);
+        soldTokensTotalSupply -= amount_;
     }
 
     /// Utils
-    function secondsPerDay(uint256 second) private pure returns (uint256) {
-        return (second / (86400) + 1);
+    function secondsPerDay(uint256 second_) private pure returns(uint256) {
+
+        return (second_ / 86400);
     }
  
     /// Owners API
-    function currentStatus(address owner) public view returns (Owners memory) {
-        require(msg.sender != owners[owner].owner, "you can not access this information");
-      
-        return owners[owner];
+    function currentStatus(address owner_) public view returns(Owners memory) {
+
+        require(msg.sender != owners[owner_].owner, "you can not accsess this information");
+        return owners[owner_];
     }
 
     function ownersToUse() public payable {
-        require(msg.sender == owners[msg.sender].owner, "you can not access this information");
+
+        require(msg.sender == owners[msg.sender].owner, "you can not accsess this information");
         require(owners[msg.sender].currentTime > block.timestamp, "Your account is still frozen");
-       
-        uint256 ownerPercent =  (owners[msg.sender].user == OwnerType.Employee ? 10000 : 50000); 
+
+        /// check employee or Founder
+        uint256 ownerPercent = (owners[msg.sender].user == OwnerType.Employee ? 10000 : 50000); 
 
         uint256 day = secondsPerDay((block.timestamp - owners[msg.sender].currentTime));
         uint256 partOfAmount = (day / 30);
